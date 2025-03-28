ef033327e78a55176afd245905bf73ccaefb0dbd
kumarn <naveenkmrhn@gmail.com>
2025-03-28 06:21:10 +0530

    Use Blueprint

--- weight-on-planets/nosql_be/app.py
+++ weight-on-planets/nosql_be/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Blueprint
 from flask_cors import CORS
 from pymongo import MongoClient
 
@@ -11,11 +11,17 @@ client = MongoClient(MONGO_URI)
 db = client["planets_db"]  # Database name
 collection = db["planets"]  # Collection name
 
+# Create Blueprint
+api = Blueprint('api', __name__)
+
 @app.route('/planets', methods=['GET'])
 def get_planets():
     planets = collection.find({}, {"_id": 0, "name": 1, "gravity": 1})  # Exclude '_id' field
     planets_data = {planet["name"]: planet["gravity"] for planet in planets}
     return jsonify(planets_data)
+
+# Register blueprint with a URL prefix
+app.register_blueprint(api, url_prefix='/wt-converter-be')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
